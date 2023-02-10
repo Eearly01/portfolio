c644f305214e92e0becfa44f62b19f75fcb0c04e
eearly01 <elijah.early@gmail.com>
2023-02-10 12:24:56 -0600

    carousel images working but still buggy

--- app.js
+++ app.js
@@ -1,13 +1,76 @@
-document.addEventListener("scroll", function () {
-  const navbar = document.querySelector(".nav-bar");
-  const navHeight = 200;
-  const distanceFromTop = Math.abs(
-    document.body.getBoundingClientRect().top);
-  if (distanceFromTop >= navHeight)
-  {
-     navbar.classList.add("moving-nav");
+$(() => {
+  document.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".nav-bar");
+    const navHeight = 200;
+    const distanceFromTop = Math.abs(
+      document.body.getBoundingClientRect().top);
+    if (distanceFromTop >= navHeight)
+    {
+      navbar.classList.add("moving-nav");
+    }
+    else {
+      navbar.classList.remove("moving-nav");
+    }
+  }); //https://markoskon.com/fixed-navbar-on-top-with-position-sticky/
+
+  let slideIndex = 1;
+
+  showSlides(slideIndex);
+
+  $('.next').on('click',function() {
+    showSlides(slideIndex += 1);
+  })
+
+  $('.prev').on('click',function() {
+    showSlides(slideIndex -= 1);
+  })
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
   }
-  else {
-    navbar.classList.remove("moving-nav");
+
+  function showSlides(n) {
+    let i;
+    let slides = $(".carousel");
+    if (n > slides.length) 
+      {slideIndex = 1}
+    if (n < 1) 
+      {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides.eq(slideIndex-1).css("display","block");
+
   }
-}); //https://markoskon.com/fixed-navbar-on-top-with-position-sticky/
+
+  const cContainerF = $(".carousel-containerF");
+  const cContainerB = $(".carousel-containerB");
+  const $btnF = $("#btnFire");
+  const $btnB = $("#btnBackpack");
+
+  $btnF.on('click', function () {
+  slideIndex = 1;
+  console.log(cContainerF.css('display'))
+    if (cContainerF.is(':visible')) {
+      cContainerF.css('display','none');
+      //console.log('click!')
+    } else {
+      cContainerF.css('display','block');
+
+    }
+  });
+
+    $btnB.on('click', function () {
+  console.log(cContainerB.css('display'))
+  slideIndex = 1;
+    if (cContainerB.is(':visible')) {
+      cContainerB.css('display','none');
+      //console.log('click!')
+    } else {
+      cContainerB.css('display','block');
+
+    }
+  });
+
+
+})
